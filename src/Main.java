7c0f2ed812466c77fa280eb206db0d8bc60782cd
Munir Safi <munir.safi87@gmail.com>
2023-04-03 07:40:38 -0500

    the basic of solar system is ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem soler=new SolarSystem();
+        soler.sun="1";
+        soler.moon="2";
+        soler.planet="3";
+
+    }
+}
